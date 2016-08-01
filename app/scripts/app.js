a42b27a9b138b5ac997236012449d45689392794
briansmont <briansmont830@gmail.com>
2016-08-01 10:52:33 -0400

    Angular checkpoint/assignment3, revised location of ng-app code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,26 @@
- (function() {
-     function config($stateProvider, $locationProvider) {
+(function() {
+    function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-                enabled: true;
-                requireBase: false;
-        });
+                enabled: true,
+                requireBase: false
+            });
+
         $stateProvider
             .state('landing', {
                 url: '/',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
-                url:'/',
-                templateUrl: '/templates/album.html'
+                url: '/album',
+                templateUrl: 'templates/album.html'
             })
             .state('collection', {
-                url:'/',
-                templateURL: '/templates/collection.html'
+                url: '/collection',
+                templateUrl: 'templates/collection.html'
             });
-     }
- 
-     angular
-         .module('blocJams', ['ui.router'])
-         .config(config);
- })();
+    }
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,23 +1,26 @@
- (function() {
-     function config($stateProvider, $locationProvider) {
+(function() {
+    function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-                enabled: true;
-                requireBase: false;
-        });
+                enabled: true,
+                requireBase: false
+            });
+
         $stateProvider
             .state('landing', {
                 url: '/',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
-                url:'/',
-                templateUrl: '/templates/album.html'
+                url: '/album',
+                templateUrl: 'templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: 'templates/collection.html'
             });
-        
-     }
- 
-     angular
-         .module('blocJams', ['ui.router'])
-         .config(config);
- })();
+    }
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
