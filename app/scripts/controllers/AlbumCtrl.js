dc2742c6d353b2ed4b398fd411da75fc9d6f68e0
briansmont <briansmont830@gmail.com>
2016-08-03 18:47:04 -0400

    AngularAssignment6 - Add Service to CollectionCntrl, Fixtures now include getCollection()

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,8 +2,8 @@
     function AlbumCtrl(Fixtures) {
         this.albumData = Fixtures.getAlbum();
         this.songs = [];
-        for (var i = 0; i < this.currentAlbum.songs.length; i++) {
-            this.songs.push(this.currentAlbum.songs[i]);
+        for (var i = 0; i < this.albumData.songs.length; i++) {
+            this.songs.push(this.albumData.songs[i]);
         }
     }
     
